5e8e2f5c50f50cce320e54a398a7e66c4d53baa5
Pieter <@>
2016-11-20 20:09:12 +0100

    Ensure new out-of-band members do not conflict with new-style groups

--- admin_create_oob_member.py
+++ admin_create_oob_member.py
@@ -15,15 +15,16 @@ l,s = mdb.get_connectors()
 
 if __name__ == "__main__":
     # Parse arguments
-    usage = "./admin_create_oob_member.py <memberID> <full name> <email> <department>"
+    usage = "./admin_create_oob_member.py <memberID> <full name> <email> <department> <username>"
     parser = OptionParser(usage)
     (options, args) = parser.parse_args()
-    if len(args) != 4:
-        parser.error("I require four arguments")
+    if len(args) != 5:
+        parser.error("I require five arguments")
     memberId = str(args[0])
     fullName = str(args[1])
     email = str(args[2])
     department = str(args[3])
+    username = str(args[4])
 
     # Check validity of arguments
     if not Member.is_valid_lidnummer(memberId):
@@ -34,6 +35,8 @@ if __name__ == "__main__":
         parser.error("Not a valid email address. Aborting...")
     if not Member.is_valid_afdeling(department):
         parser.error("Not a valid department. Aborting...")
+    if not Member.is_valid_username(username):
+        parser.error("Not a valid username. Aborting...")
     newMember = Member.Member(l,s,int(memberId))
     if newMember.exists():
         helper.logger.error("Member %s already exists. Aborting..." % memberId)
@@ -41,6 +44,7 @@ if __name__ == "__main__":
 
     # Create out-of-band member
     newMember.create(fullName, email, department)
+    newMember.make_user(username, 3)
     group = Group.Group(l,s,"type-outofband")
     group.add(newMember)
     helper.logger.info("Created out-of-band member %s (%s)." % (fullName, memberId))
